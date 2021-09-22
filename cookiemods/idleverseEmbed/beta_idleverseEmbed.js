f927cf612eda02d0f2971897a6c9ffb722a66879
Enderspearl184 <73859349+Enderspearl184@users.noreply.github.com>
2021-09-21 21:29:29 -0700

    Create beta_idleverseEmbed.js

---
+++ cookiemods/idleverseEmbed/beta_idleverseEmbed.js
@@ -0,0 +1,40 @@
+if(Game.Objects['Idleverse'].minigame) throw new Error("Unable to load Idleverse minigame over itself.");
+
+var M = {};
+M.parent = Game.Objects['Idleverse'];
+M.parent.minigame = M;
+M.loadedCount = 0;
+M.version = '3.16';
+M.GameVersion = '2.031';
+
+M.launch = function(){
+    console.log("launch")
+	var M = this;
+
+	M.init = function(div){
+    div.height="968"
+		var str = '';
+		str += '<iframe height=100% width=100% src="https://orteil.dashnet.org/experiments/cookie"></iframe>';
+		div.innerHTML = str;
+		M.loadedCount++;
+		if (Game.prefs.popups) Game.Popup('Minigame loaded!');
+		else Game.Notify('Minigame loaded!', '', '', 1, 1);
+	}
+	
+	M.init(l('rowSpecial' + M.parent.id));
+}
+
+
+M.launcher = function(){
+    console.log("launcher")
+	var M = Game.Objects['Idleverse'].minigame;
+	M.parent.minigameName = 'Classic Clicker';
+	M.parent.minigameUrl = 'https://enderspearl184.github.io/cookiemods/idleverseEmbed/dummy.js'
+	M.name = M.parent.minigameName;
+	M.savePrefix = 'minigameClicker';
+	Game.LoadMinigames();
+}
+
+M.launcher();
+
+var M = 0;
