982b7908028498c572c888672b29eca1af285c4d
Enderspearl184 <73859349+Enderspearl184@users.noreply.github.com>
2021-10-09 14:26:46 -0700

    Create halloween2021style.user.js

---
+++ brickhill/userscripts/halloween2021style.user.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name         Brick Hill Halloween 2021 Style
+// @version      0.1
+// @description  Keep the 2021 Halloween Style Forever!
+// @author       You
+// @match        https://*.brick-hill.com/*
+// @icon         https://www.google.com/s2/favicons?domain=www.brick-hill.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    function addGlobalStyle(href) {
+        var head, style;
+        head = document.getElementsByTagName('head')[0];
+        if (!head) { return; }
+        style = document.createElement('link');
+        style.rel = 'stylesheet';
+        style.type = 'text/css';
+        style.href = href;
+        head.appendChild(style);
+    }
+    document.getRootNode().children[0].className='theme-halloween';
+    for (let link of document.querySelectorAll('link')) {
+        if (link.rel=="stylesheet") {
+            link.remove();
+        }
+    }
+    addGlobalStyle("https://enderspearl184.github.io/brickhill/css/halloween2021.css");
+    addGlobalStyle("https://enderspearl184.github.io/brickhill/css/halloween2021main.css");
+})();
