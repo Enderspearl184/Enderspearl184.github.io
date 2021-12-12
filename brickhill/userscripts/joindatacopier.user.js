ccf2b4da7aa0c7cc2a96098b3d459359e812b47a
Enderspearl184 <73859349+Enderspearl184@users.noreply.github.com>
2021-12-12 15:12:34 -0800

    Update joindatacopier.user.js

--- brickhill/userscripts/joindatacopier.user.js
+++ brickhill/userscripts/joindatacopier.user.js
@@ -4,69 +4,26 @@
 // @version      0.1
 // @author       Enderspearl184
 // @match        https://*.brick-hill.com/play/*
+// @run-at       document-idle
 // @grant        none
 // ==/UserScript==
-function main() {
-	const placeData = {
-		ip: window.BH.apps.PlayButton.$children[0].ip,
-		port: window.BH.apps.PlayButton.$children[0].port,
-		id: window.BH.apps.PlayButton.$children[0].set_id,
-		play: window.BH.apps.PlayButton.$children[0].play,
-	}
+setTimeout(function(){
+const stuff = {
+    address: window.BH.apps.SetPage.$children[0].setIp,
+    id: window.BH.apps.SetPage.$children[0].setId,
+    port: window.BH.apps.SetPage.$children[0].setPort
+}
+const playButton = document.getElementsByClassName('play-button')[0];
+const copyButton = playButton.cloneNode();
 
-	// The game is not currently active
-	if (!placeData.play) return
-
-	// Inject buttons
-	const buttonDiv = document.querySelector('div#playbutton-v')
-	const brickPlayerButton = document.createElement('button')
-	const buttonContent = document.createTextNode('Copy Join Data')
-
-	brickPlayerButton.appendChild(buttonContent)
-	brickPlayerButton.classList.add('blue', 'mb2')
-	buttonDiv.appendChild(brickPlayerButton)
-
-	// Handle launch
-	function brickPlayerConnect() {
-		console.log('Launching game...')
-		fetch('https://api.brick-hill.com/v1/auth/generateToken?set='+placeData.id, {
-			credentials: 'include'
-		})
-			.then((res => res.json()))
-			.then((data) => {
-				const token = data.token
-				navigator.clipboard.writeText(`{"token":"${token}","id":"${placeData.id}","port":"${placeData.port}","ip":"${placeData.ip}"}`)
-			})
-	}
-
-	brickPlayerButton.onclick = brickPlayerConnect
+copyButton.className = copyButton.className.replace('play-button ', '').replace('green', 'red');
+copyButton.textContent = 'Copy Join Data';
+copyButton.onclick = () => {
+    window.axios.get(window.BH.apiUrl('v1/auth/generateToken?set='.concat(stuff.id)))
+        .then(function({data}){
+            navigator.clipboard.writeText(`{"token":"${data.token}","id":"${stuff.id}","port":"${stuff.port}","ip":"${stuff.address}"}`)
+        });
 }
 
-function inject(source) {
-	const script = document.createElement('script')
-	script.text = `(${source})()`
-	document.documentElement.appendChild(script)
-}
-
-(function() {
-    'use strict';
-    	const target = document.querySelector('div#playbutton-v')
-	// Button already exists
-	if (target) {
-		console.log('Target already exists')
-		return inject(main)
-	}
-
-	// Wait for the play button to be added to the dom
-	const observer = new MutationObserver(() => {
-		if (document.contains(document.querySelector('div#playbutton-v'))) {
-			observer.disconnect()
-			return inject(main)
-		}
-	})
-
-	observer.observe(document, {
-		childList: true,
-		subtree: true,
-	})
-})();
+playButton.parentNode.append(copyButton);
+},500)
