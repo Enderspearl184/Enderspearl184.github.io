1077d7449e9ab5a906fd2dba6f8789cf76a4049e
Enderspearl184 <73859349+Enderspearl184@users.noreply.github.com>
2022-05-11 22:30:30 -0700

    Update purchasedonator.user.js

--- roblox/userscripts/purchasedonator.user.js
+++ roblox/userscripts/purchasedonator.user.js
@@ -1,12 +1,14 @@
 // ==UserScript==
 // @name         purchase donator
-// @version      0.2
-// @description  gives me (or other people) the commission bonus when you buy catalog items (except t-shirts, shirts, pants, and bundles)
+// @version      0.3
+// @description  gives me your money for buying stuff on-site thanks
 // @author       Enderspearl184
 // @match        https://www.roblox.com/catalog/*
 // @match        https://web.roblox.com/catalog/*
 // @match        https://www.roblox.com/game-pass/*
 // @match        https://web.roblox.com/game-pass/*
+// @match        https://www.roblox.com/games/*
+// @match        https://web.roblox.com/games/*
 // @icon         https://www.google.com/s2/favicons?domain=roblox.com
 // @run-at       document-end
 // @grant        none
@@ -16,27 +18,31 @@
 //it MIGHT work on limiteds (untested) but i don't think you can buy items in game so probably not
 
 
-const PLACE_ID = 8910542885; //the place id to send as the sale origin, gives the place owner 40% of the item cost
+const PLACE_ID = 9336310638; //the place id to send as the sale origin, gives the place owner 40% of the item cost
                              //also, it probably doesn't give you the commission if the place is owned by you because roblox isn't dumb, maybe try it with a group game or something idk.
                              //the only items that i KNOW don't give 40% of the cost are t-shirts, shirts, pants, and bundles.
 
 
 (function() {
-    let button = document.querySelector("button.PurchaseButton") //find purchase button
-    if (button) {
+    setInterval(()=>{
+    let buttons = document.querySelectorAll("button.PurchaseButton") //find purchase button
+    buttons.forEach((button)=>{
+        if (button.modified) return
+        console.log("modifying button")
+        button.modified=true
         button.onclick = function() { //when you click it
             setTimeout(function() {
                 let confirm = document.getElementById("confirm-btn") //find the confirm button for purchasing
-                let confirmParent = confirm.parentElement //find it's parent
+                let confirmParent = confirm.parentElement //find its parent
                 confirm.remove() //goodbye confirm button rip bozo
                 //add the button back but without the normal confirm button script firing
-                confirmParent.innerHTML = `<a href="" id="confirm-btn" class="btn-primary-md">Get Now</a>` + confirmParent.innerHTML + `<br><br><div class="modal-footer text-footer modal-footer-center">If eligible, 40% of the cost will go to the creator of place <a href=/games/${PLACE_ID}>${PLACE_ID}</a></div>`
+                confirmParent.innerHTML = `<a id="confirm-btn" class="btn-primary-md">Get Now</a>` + confirmParent.innerHTML + `<br><br><div class="modal-footer text-footer modal-footer-center">If eligible, 40% of the cost will go to the creator of place <a href=/games/${PLACE_ID}>${PLACE_ID}</a></div>`
                 confirmParent.children[0].onclick = function() { //when you click the confirm button do this
                     fetch(`https://economy.roblox.com/v1/purchases/products/${button.dataset.productId}`, {
                             body: JSON.stringify({
                                 expectedCurrency: 1, //expectedcurrency 1 is just robux
                                 expectedPrice: button.dataset.expectedPrice, //pretty self-explanatory
-                                expectedSellerId: 0, //idk why its 0 here but that's how it gets sent in game so idc
+                                expectedSellerId: button.dataset.expectedSellerId, //0, //idk why its 0 here but that's how it gets sent in game so idc
                                 expectedPromoId: 0, //also 0, just how it gets sent in game
                                 saleLocationId: PLACE_ID, //send the place id as the sale location
                                 saleLocationType: "Game", //and also that it was in a game
@@ -50,10 +56,11 @@ const PLACE_ID = 8910542885; //the place id to send as the sale origin, gives th
                             }
                         })
                         .then(() => {
-                            location = location.href //actually reload the page because for some reason location.reload() doesn't change the item owned sign.
+                            location.reload(true) //actually reload the page because for some reason location.reload() doesn't change the item owned sign.
                         })
                 }
             }, 25)
         }
-    }
+    })
+    },1000)
 })();
